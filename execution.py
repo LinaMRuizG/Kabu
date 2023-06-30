fe003069703121914b51c6ef74edacdb6b885220
linar <linar@wolfram.com>
2023-06-30 12:12:10 -0500

    adding files

--- execution.py
+++ execution.py
@@ -5,14 +5,14 @@ import pandas as pd
 
 if __name__ == "__main__":
     
-    database = pd.read_csv("/Users/linaruiz/Documents/projectEpidemicCurve/data/database.csv")
+    database = pd.read_csv("/Users/linaruiz/Documents/EpidemicKabu_project/dataGeneral/databaseExampleItaly.csv")
     datesName = "Date_reported"
     casesName = "New_cases"
     kernel = 28
-    plotName = "Epidemic curve for Italy"
-    dfName = "Epidemic_curve_Italy"
-    outFolderPlot = "/Users/linaruiz/Documents/projectEpidemicCurve/kabu/Kabu/plots/"
-    outFolderDF = "/Users/linaruiz/Documents/projectEpidemicCurve/kabu/Kabu/dataframes/"
+    plotName = "Epidemic curve for Italy "
+    dfName = "Epidemic_curve_Italy_"
+    outFolderPlot = "/Users/linaruiz/Documents/EpidemicKabu_project/EpidemicKabu/plots/"
+    outFolderDF = "/Users/linaruiz/Documents/EpidemicKabu_project/EpidemicKabu/dataframes/"
     thresholdW = 0
     thresholdPV = 0
 
@@ -21,15 +21,15 @@ if __name__ == "__main__":
     test.runAndPlot()
     
     #for waves class
-    #test = waves(database,datesName,casesName,kernel,plotName,dfName,outFolder,outFolderPlot,outFolderDF)
+    test = waves(database,datesName,casesName,kernel,plotName + "W",dfName +"W",outFolderPlot,outFolderDF)
+    test.run() 
+    #test = waves(database,datesName,casesName,kernel,plotName + "W",dfName +"W")
     #test.run() 
-    #test2 = waves(database,datesName,casesName,kernel,plotName,dfName)
-    #test2.run() 
 
     #for peaksValleys class
-    #test = peaksValleys(database,datesName,casesName,kernel,plotName,dfName,outFolder,outFolderPlot,outFolderDF)
+    test = peaksValleys(database,datesName,casesName,kernel,plotName + "PV",dfName + "PV",outFolderPlot,outFolderDF)
+    test.run() 
+    #test = peaksValleys(database,datesName,casesName,kernel,plotName + "PV",dfName + "PV")
     #test.run() 
-    #test2 = peaksValleys(database,datesName,casesName,kernel,plotName,dfName)
-    #test2.run() 
     
     
